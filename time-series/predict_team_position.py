4a5c7f502e351f8d48de3f64cf55de429bd32d1d
Marcos Paulo Almeida <41038049+Marcos14Almeida@users.noreply.github.com>
2023-06-18 11:21:03 -0300

    update files

--- time-series/predict_team_position.py
+++ time-series/predict_team_position.py
@@ -30,11 +30,35 @@ data = {
     'Torino':     [28, 22, 16, 7, 9, 12, 9, 9, 7, 16, 17, 10, 10],
     'Bologna':    [16, 9, 13, 19, 23, 14, 15, 15, 10, 12, 12, 13, 9],
     'Sassuolo':   [36, 24, 21, 17, 12, 6, 12, 11, 11, 8, 8, 11, 13],
-    'Udinese':   [4, 3, 5, 13, 16, 17, 13, 14, 12, 14, 14, 12, 12]
+    'Udinese':    [4, 3, 5, 13, 16, 17, 13, 14, 12, 14, 14, 12, 12]
+}
+
+data_premier = {
+    'Season': [
+        '2010/2011', '2011/2012', '2012/2013', '2013/2014', '2014/2015',
+        '2015/2016', '2016/2017', '2017/2018', '2018/2019', '2019/2020',
+        '2020/2021', '2021/2022', '2022/2023'],
+    'Arsenal':     [4, 3, 4, 4, 3,   2, 6, 6, 5, 8,   8, 5, 2],
+    'Aston Villa': [9, 16, 15, 15, 15,    17, 15, 17, 13, 17, 11, 14, 7],
+    'Brighton':    [41, 30, 24, 26, 40,    23, 22, 15, 17, 15, 16, 9, 5],
+    'Chelsea':     [2, 6, 3, 3, 1,   10, 1, 5, 3, 4,   4, 3, 12],
+    # 'Crystal Palace':    [40, 37, 25, 11, 10,   15, 15, 11, 12, 14,  14, 12, 11],
+    'Everton':     [7, 7, 6, 5, 11,  11, 8, 8, 8, 12,   9, 16, 17],
+    'Leicester':   [30, 29, 26, 21, 14,    1, 13, 9, 9, 5,     5, 8, 18],
+    'Liverpool':   [6, 8, 7, 2, 6,   8, 4, 4, 2, 1,   3, 2, 6],
+    'Man City':    [3, 1, 2, 1, 2,   1, 4, 3, 1, 1,   2, 1, 1],
+    'Man Utd':     [1, 2, 1, 7, 4,   5, 7, 2, 7, 3,   2, 6, 3],
+    'Newcastle':   [12, 5, 16, 10, 15,   18, 21, 10, 13, 13,   12, 11, 4],
+    'Southampton': [41, 22, 14, 8, 7,    6, 9, 17, 16, 11,     15, 15, 20],
+    'Stoke City':  [13, 14, 13, 9, 9,    9, 14, 19, 36, 35,    34, 34, 36],
+    'Tottenham':   [7, 4, 5, 4, 6,       5, 3, 3, 4, 6,        7, 4, 8],
+    'West Bromwich': [11, 10, 8, 17, 13,   14, 11, 20, 24, 22, 19, 30, 29],
+    'West Ham':    [20, 23, 10, 13, 12,    7, 12, 13, 10, 16,  6, 7, 14],
+    'Wolves':      [17, 20, 42, 41, 27,    34, 35, 21, 7, 7,   13, 10, 13],
 }
 
 
-def plot():
+def plot(data):
     # Extract the clubs' names for the legend
     clubs = list(data.keys())[1:]
 
@@ -72,14 +96,16 @@ def reorder_df_predictions(predicted_positions, initial_year):
     return df
 
 
-plot()
+data = data_premier
+
+plot(data)
 
 df = pd.DataFrame(data)
 
 # Check for data stationarity.
 # P-value must be < 0.05
 # Perform ADF test on the 'Napoli' column
-result = adfuller(df['Napoli'])
+result = adfuller(df['Arsenal'])
 # Print the ADF test statistic
 print("ADF Statistic:", result[0])
 # Print the p-value
